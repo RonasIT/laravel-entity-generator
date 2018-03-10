360c4f621fccb1068ffb36492d8b8405a94e10b1
Roman Vashchenko <rvashchenko@ronasit.com>
2018-03-10 17:47:00 +0600

    #13: fixed routes middleware in template

--- stubs/routes.blade.php
+++ stubs/routes.blade.php
@@ -1,6 +1,6 @@
 
-Route::post('/{{$entities}}', ['uses' => {{$entity}}Controller::class.'@create'])->middleware('auth:api');
-Route::put('/{{$entities}}/{id}', ['uses' => {{$entity}}Controller::class.'@update'])->middleware('auth:api');
-Route::delete('/{{$entities}}/{id}', ['uses' => {{$entity}}Controller::class.'@delete'])->middleware('auth:api');
+Route::post('/{{$entities}}', ['uses' => {{$entity}}Controller::class.'@create'])->middleware('jwt.auth');
+Route::put('/{{$entities}}/{id}', ['uses' => {{$entity}}Controller::class.'@update'])->middleware('jwt.auth');
+Route::delete('/{{$entities}}/{id}', ['uses' => {{$entity}}Controller::class.'@delete'])->middleware('jwt.auth');
 Route::get('/{{$entities}}/{id}', ['uses' => {{$entity}}Controller::class.'@get']);
 Route::get('/{{$entities}}', ['uses' => {{$entity}}Controller::class.'@search']);
