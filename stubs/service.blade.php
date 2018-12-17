998d154f98c096fc49166b8865b9491572b4eb30
susov <susov@ronasit.com>
2018-12-17 23:51:07 +0600

    #-: Fix indents in PHPDocs generated

--- stubs/service.blade.php
+++ stubs/service.blade.php
@@ -2,10 +2,18 @@ namespace App\Services;
 
 use App\Repositories\{{$entity}}Repository;
 use RonasIT\Support\Services\EntityService;
-
+{{--
+    Laravel inserts two spaces between @property and type, so we are forced
+    to use hack here to preserve one space
+--}}
+@php
+echo <<<PHPDOC
 /**
- * @property {{$entity}}Repository $repository
+ * @property {$entity}Repository \$repository
  */
+
+PHPDOC;
+@endphp
 class {{$entity}}Service extends EntityService
 {
     public function __construct()
