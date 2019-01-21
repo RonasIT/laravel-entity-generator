b7daf6c1c552e6bba8cf1d825b85ff716c03cb5f
Roman Dubrovin <rdubrovin@ronasit.com>
2019-01-21 13:31:43 +0600

    #-: fixed templates

--- stubs/dumps/mysql.blade.php
+++ stubs/dumps/mysql.blade.php
@@ -1,6 +1,7 @@
 @foreach($inserts as $entities)
 @foreach($entities['items'] as $entity)
-INSERT INTO {{$entities['name']}}({!! implode(', ', $entity['fields']) !!}, created_at, updated_at)
-    VALUES({!! implode(', ', $entity['values']) !!}, '2016-10-20 11:05:00', '2016-10-20 11:05:00');
+INSERT INTO {{$entities['name']}}({!! implode(', ', $entity['fields']) !!}, created_at, updated_at) VALUES
+  ({!! implode(', ', $entity['values']) !!}, '2016-10-20 11:05:00', '2016-10-20 11:05:00');
 @endforeach
+
 @endforeach
