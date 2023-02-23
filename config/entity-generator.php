28895f7cd109801bf84311ef9689a54d5e24655f
roman2027@internet.ru <roman2027@internet.ru>
2023-02-23 15:17:50 +0600

    feat: add nova field to config Refs: #19

--- config/entity-generator.php
+++ config/entity-generator.php
@@ -15,6 +15,7 @@ return [
         'factory' => 'database/factories/ModelFactory.php',
         'translations' => 'resources/lang/en/validation.php',
         'resources' => 'app/Http/Resources',
+        'nova' => 'app/Nova',
         'nova_provider' => 'app/Providers/NovaServiceProvider.php'
      ],
     'stubs' => [
