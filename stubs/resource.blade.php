20add5a9465339c357dcb82a96a275a0c7f9cd86
dail <aausagit@ronasit.com>
2023-07-05 19:12:26 +0600

    fix: added todo comment

--- stubs/resource.blade.php
+++ stubs/resource.blade.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class {{$entity}}Resource extends JsonResource
 {
+    //TODO implement custom serialization logic or remove method redefining
     public function toArray($request): array
     {
         return parent::toArray($request);
