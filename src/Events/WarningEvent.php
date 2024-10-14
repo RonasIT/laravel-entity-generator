ab647ee197dd223c979cc2c26cb1851d821f3dac
DenTray <dpankratov@ronasit.com>
2024-10-14 17:14:47 +0600

    Update src/Events/WarningEvent.php

--- src/Events/WarningEvent.php
+++ src/Events/WarningEvent.php
@@ -8,11 +8,6 @@ class WarningEvent
 {
     use SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
     public function __construct(
         public readonly string $message,
     ) {
