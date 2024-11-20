04a4bad3e8fb7afe7049eca3008669862652a1fa
roman <roman2027@internet.ru>
2024-11-20 17:48:34 +0600

    feat: add property annotation refs: https://github.com/RonasIT/laravel-entity-generator/issues/103

--- stubs/resource.blade.php
+++ stubs/resource.blade.php
@@ -1,7 +1,11 @@
 namespace {{$namespace}};
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use {{$model_namespace}}\{{$entity}}
 
+/**
+  *@property {{$entity}} $resource
+ */
 class {{$entity}}Resource extends JsonResource
 {
     //TODO implement custom serialization logic or remove method redefining
