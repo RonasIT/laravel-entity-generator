540e864e438240bd7c3cd4754d6f93499d1b3ad2
roman <roman2027@internet.ru>
2024-11-28 15:14:14 +0600

    fix: code refs: https://github.com/RonasIT/laravel-entity-generator/issues/100

--- src/Generators/AbstractTestsGenerator.php
+++ src/Generators/AbstractTestsGenerator.php
@@ -264,40 +264,4 @@ abstract class AbstractTestsGenerator extends EntityGenerator
             self::UPDATED_AT,
         ]);
     }
-
-    public function extractNamespace(string $file) : string
-    {
-        $contents = file_exists($file) ? file_get_contents($file) : $file;
-        $namespace = $class = '';
-        $getting_namespace = $getting_class = false;
-
-        foreach (token_get_all($contents) as $token) {
-
-            if (is_array($token) && $token[0] == T_NAMESPACE) {
-                $getting_namespace = true;
-            }
-
-            if (is_array($token) && $token[0] == T_CLASS) {
-                $getting_class = true;
-            }
-
-            // While we're grabbing the namespace name...
-            if ($getting_namespace === true) {
-                if (is_array($token) && in_array($token[0], [T_STRING, T_NS_SEPARATOR])) {
-                    $namespace .= $token[1];
-                } else if ($token === ';') {
-                    $getting_namespace = false;
-                }
-            }
-
-            if ($getting_class === true) {
-                if (is_array($token) && $token[0] == T_STRING) {
-                    $class = $token[1];
-                    break;
-                }
-            }
-        }
-
-        return $namespace ? $namespace . '\\' . $class : $class;
-    }
 }
