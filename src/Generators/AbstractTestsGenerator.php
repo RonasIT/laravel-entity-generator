f397ce6e0d896836c2f8dc2eea5e114240844f74
roman <roman2027@internet.ru>
2024-11-28 11:00:40 +0600

    refactor: get model namespace from path refs: https://github.com/RonasIT/laravel-entity-generator/issues/100

--- src/Generators/AbstractTestsGenerator.php
+++ src/Generators/AbstractTestsGenerator.php
@@ -163,7 +163,9 @@ abstract class AbstractTestsGenerator extends EntityGenerator
 
     protected function getModelClass($model): string
     {
-        return "App\\Models\\{$model}";
+        $path = "{$this->paths['models']}/{$model}";
+
+        return Str::ucfirst(str_replace('/', '\\', trim($path, '/')));
     }
 
     protected function getModelFields($model): array
@@ -261,5 +263,41 @@ abstract class AbstractTestsGenerator extends EntityGenerator
             self::CREATED_AT,
             self::UPDATED_AT,
         ]);
+    }
+
+    public function extractNamespace(string $file) : string
+    {
+        $contents = file_exists($file) ? file_get_contents($file) : $file;
+        $namespace = $class = '';
+        $getting_namespace = $getting_class = false;
+
+        foreach (token_get_all($contents) as $token) {
+
+            if (is_array($token) && $token[0] == T_NAMESPACE) {
+                $getting_namespace = true;
+            }
+
+            if (is_array($token) && $token[0] == T_CLASS) {
+                $getting_class = true;
+            }
+
+            // While we're grabbing the namespace name...
+            if ($getting_namespace === true) {
+                if (is_array($token) && in_array($token[0], [T_STRING, T_NS_SEPARATOR])) {
+                    $namespace .= $token[1];
+                } else if ($token === ';') {
+                    $getting_namespace = false;
+                }
+            }
+
+            if ($getting_class === true) {
+                if (is_array($token) && $token[0] == T_STRING) {
+                    $class = $token[1];
+                    break;
+                }
+            }
+        }
+
+        return $namespace ? $namespace . '\\' . $class : $class;
     }
 }
