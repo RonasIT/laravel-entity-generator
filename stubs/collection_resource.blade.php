90fa94bb77c32e9b390a45d1314351a37a93f7bb
DenTray <dpankratov@ronasit.com>
2024-12-16 16:17:55 +0600

    Update stubs/collection_resource.blade.php

--- stubs/collection_resource.blade.php
+++ stubs/collection_resource.blade.php
@@ -4,7 +4,5 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class {{$plural_name}}CollectionResource extends ResourceCollection
 {
-    public static $wrap = null;
-
     public $collects = {{$singular_name}}Resource::class;
 }
