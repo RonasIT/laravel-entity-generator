3f66a2fcb089ec930ceaa3f7648da3a57edcf280
roman <roman2027@internet.ru>
2024-12-18 12:20:23 +0600

    feat: change tests refs: https://github.com/RonasIT/laravel-entity-generator/issues/49

--- tests/RepositoryGeneratorTest.php
+++ tests/RepositoryGeneratorTest.php
@@ -14,9 +14,11 @@ class RepositoryGeneratorTest extends TestCase
     {
         $this->mockGeneratorForMissingModel();
 
-        $this->expectException(ClassNotExistsException::class);
-        $this->expectErrorMessage("Cannot create Post Model cause Post Model does not exists. "
-            . "Create a Post Model by himself or run command 'php artisan make:entity Post --only-model'.");
+        $this->assertExceptionThrew(
+            className: ClassNotExistsException::class,
+            message: "Cannot create Post Model cause Post Model does not exists. "
+            . "Create a Post Model by himself or run command 'php artisan make:entity Post --only-model'.",
+        );
 
         app(RepositoryGenerator::class)
             ->setModel('Post')
@@ -25,15 +27,11 @@ class RepositoryGeneratorTest extends TestCase
 
     public function testCreateRepository()
     {
-        $this->mockConfigurations();
-        $this->mockViewsNamespace();
         $this->mockFilesystem();
 
         app(RepositoryGenerator::class)
             ->setModel('Post')
             ->generate();
-
-        $this->rollbackToDefaultBasePath();
 
         $this->assertGeneratedFileEquals('repository.php', 'app/Repositories/PostRepository.php');
     }
