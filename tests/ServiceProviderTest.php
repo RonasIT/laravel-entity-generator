b259c30644629663ca2e47dcd9d143b591af875f
roman <roman2027@internet.ru>
2025-02-03 18:03:47 +0600

    feat: fix tests refs: https://github.com/RonasIT/laravel-entity-generator/issues/49

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -17,12 +17,12 @@ class ServiceProviderTest extends TestCase
         $this->assertArrayHasKey('make:entity', Artisan::all());
 
         $this->assertTrue(in_array(
-            needle: '/app/vendor/orchestra/testbench-core/laravel/resources/views',
+            needle: getcwd() . '/vendor/orchestra/testbench-core/laravel/resources/views',
             haystack: $finder->getPaths(),
         ));
 
         $this->assertEquals(
-            expected: ['/app/src/../config/entity-generator.php' => 'vfs://root/config/entity-generator.php'],
+            expected: [getcwd() . '/src/../config/entity-generator.php' => 'vfs://root/config/entity-generator.php'],
             actual: EntityGeneratorServiceProvider::$publishes['RonasIT\Support\EntityGeneratorServiceProvider'],
         );
     }
