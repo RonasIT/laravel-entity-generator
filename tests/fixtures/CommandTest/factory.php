5334c5a20c07375d41ffb98d2eff41c2650bccdd
Ruslan Guskov <feniksuh@gmail.com>
2025-05-27 15:16:21 +0300

    fix:tests

--- tests/fixtures/CommandTest/factory.php
+++ tests/fixtures/CommandTest/factory.php
@@ -2,6 +2,7 @@
 
 namespace RonasIT\Support\Tests\Support\Command\Factories;
 
+use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use RonasIT\Support\Tests\Support\Command\Models\Post;
 
