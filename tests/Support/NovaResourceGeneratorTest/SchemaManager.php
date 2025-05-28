d90871d11c01ba58f2db0ecff9fa006587465da7
Ruslan Guskov <feniksuh@gmail.com>
2025-05-28 16:16:15 +0300

    fix: revert remove SchemaManager file

---
+++ tests/Support/NovaResourceGeneratorTest/SchemaManager.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace RonasIT\Support\Tests\Support\NovaResourceGeneratorTest;
+
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\PostgreSQLSchemaManager;
+use Doctrine\DBAL\Types\DateTimeType;
+use Doctrine\DBAL\Types\IntegerType;
+use Doctrine\DBAL\Types\StringType;
+
+class SchemaManager extends PostgreSQLSchemaManager
+{
+    public function listTableColumns(string $table): array
+    {
+        return [
+            new Column('id', new IntegerType),
+            new Column('title', new StringType),
+            new Column('created_at', new DateTimeType),
+        ];
+    }
+}
