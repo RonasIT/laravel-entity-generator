addf3a502013d040ea488333f9958d4a7d17429c
Ruslan Guskov <feniksuh@gmail.com>
2025-06-02 14:51:40 +0300

    refactor: remove useless json extensions

--- stubs/test.blade.php
+++ stubs/test.blade.php
@@ -192,14 +192,14 @@ class {{$entity}}Test extends TestCase
         return [
             [
                 'filter' => ['all' => 1],
-                'fixture' => 'search_all.json',
+                'fixture' => 'search_all',
             ],
             [
                 'filter' => [
                     'page' => 2,
                     'per_page' => 2,
                 ],
-                'fixture' => 'search_by_page_per_page.json',
+                'fixture' => 'search_by_page_per_page',
             ],
         ];
     }
