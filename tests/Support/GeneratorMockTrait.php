47aed4fbb1bb99b3178ed5cfe64268c2d5343400
Ruslan Guskov <feniksuh@gmail.com>
2025-07-09 13:43:30 +0300

    fix: remove useless

--- tests/Support/GeneratorMockTrait.php
+++ tests/Support/GeneratorMockTrait.php
@@ -41,15 +41,6 @@ trait GeneratorMockTrait
         ];
     }
 
-    public function doesNovaResourceExistsCall(bool $result = true): array
-    {
-        return [
-            'function' => 'doesNovaResourceExists',
-            'arguments' => [],
-            'result' => $result
-        ];
-    }
-
     public function nativeClassExistsMethodCall(array $arguments, bool $result = true): array
     {
         return [
