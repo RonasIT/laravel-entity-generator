414df56d2e488d62e63060d27521e09a607dde1e
pirs1337 <malygin_555@mail.ru>
2025-09-11 15:23:18 +0600

    feat: Standardize variable usage in Stub templates refs: https://github.com/RonasIT/laravel-entity-generator/issues/187

--- stubs/factory.blade.php
+++ stubs/factory.blade.php
@@ -1,14 +1,14 @@
-namespace {{$namespace}};
+namespace {{ $namespace }};
 
 @if (!empty($fields))
 use Faker\Generator as Faker;
 @endif
 use Illuminate\Database\Eloquent\Factories\Factory;
-use {{$modelNamespace}}\{{$entity}};
+use {{ $modelNamespace }}\{{ $entity }};
 
-class {{$entity}}Factory extends Factory
+class {{ $entity }}Factory extends Factory
 {
-    protected $model = {{$entity}}::class;
+    protected $model = {{ $entity }}::class;
 
     public function definition(): array
     {
@@ -18,7 +18,7 @@ class {{$entity}}Factory extends Factory
 @endif
         return [
 @foreach($fields as $field)
-            '{{$field['name']}}' => {!! \RonasIT\Support\Generators\FactoryGenerator::getFactoryFieldsContent($field) !!},
+            '{{ $field['name'] }}' => {!! \RonasIT\Support\Generators\FactoryGenerator::getFactoryFieldsContent($field) !!},
 @endforeach
         ];
     }
