2ed9b65f017706da6ff1e2cce4e4a7baa8836dad
Anton Zabolotnikov <110885041+AZabolotnikov@users.noreply.github.com>
2025-10-31 14:43:49 +0500

    Apply suggestion from @AZabolotnikov

--- tests/SeederGeneratorTest.php
+++ tests/SeederGeneratorTest.php
@@ -11,7 +11,7 @@ class SeederGeneratorTest extends TestCase
 {
     use SeederGeneratorMockTrait;
 
-    public function testCreateSeedersWhenDatabaseSeederDoesNotExist()
+    public function testCreateSeeder()
     {
         app(SeederGenerator::class)
             ->setRelations(new RelationsDTO(
