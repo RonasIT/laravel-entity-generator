27a267f69354c30eac1f08fec4a7400acd649fe5
Ni Nelli <nellini@ronasit.com>
2025-11-04 15:42:16 +0900

    test: fix tests
    refs: https://github.com/RonasIT/laravel-entity-generator/issues/217

---
+++ tests/fixtures/ModelGeneratorTest/related_model_with_property.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RonasIT\Support\Tests\Support\Models;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use RonasIT\Support\Traits\ModelTrait;
+
+/**
+ * @property Collection<Category> $categories
+ */
+class WelcomeBonus extends Model
+{
+    use ModelTrait;
+
+    public function getConnectionName(): string
+    {
+        return 'pgsql';
+    }
+
+    protected $fillable = [
+        'title',
+        'name',
+    ];
+
+    public function some_relation()
+    {
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
