eac42cb6ce783dba11bbbc8632ac9aa178736fd6
Artyom Osepyan <art@osepyan.ru>
2025-11-19 23:53:33 +0300

    refactor: update migration stub to use id instead of increments
    refs: https://github.com/RonasIT/laravel-entity-generator/issues/228

--- tests/fixtures/CommandTest/migration.php
+++ tests/fixtures/CommandTest/migration.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->timestamps();
         });
     }
